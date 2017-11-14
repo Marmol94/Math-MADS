3aca1da04fb397deeaa5ecd9fbfaa780f0d9e160
Marmol94 <michmarmol94@gmail.com>
2017-11-14 20:15:36 +0100

    Initial commit

---
+++ Math MADS/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Math_MADS
+{
+    partial class Gra
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.Panel();
+            this.Platform = new System.Windows.Forms.PictureBox();
+            this.Gracz = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Platform)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gracz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.Platform);
+            this.screen.Controls.Add(this.Gracz);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(1006, 721);
+            this.screen.TabIndex = 0;
+            // 
+            // Platform
+            // 
+            this.Platform.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Platform.Location = new System.Drawing.Point(12, 498);
+            this.Platform.Name = "Platform";
+            this.Platform.Size = new System.Drawing.Size(123, 83);
+            this.Platform.TabIndex = 1;
+            this.Platform.TabStop = false;
+            // 
+            // Gracz
+            // 
+            this.Gracz.BackColor = System.Drawing.Color.Black;
+            this.Gracz.Location = new System.Drawing.Point(31, 693);
+            this.Gracz.Name = "Gracz";
+            this.Gracz.Size = new System.Drawing.Size(25, 25);
+            this.Gracz.TabIndex = 0;
+            this.Gracz.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 30;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Gra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.screen);
+            this.Name = "Gra";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Gra_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Gra_KeyUp);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Platform)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gracz)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox Gracz;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Platform;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Math MADS/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Math_MADS
+{
+  
+    public partial class Gra : Form
+    {
+        bool prawo, lewo, skok, spadek ;
+        bool spadanie=true;
+        int force;
+        int G =30;
+        int mforce;
+
+        public void collision(PictureBox P, PictureBox G, bool s)
+        {
+            int x1, x2, x3, x4, y1, y2, y3, y4, w, h1, h2;
+            x1 = P.Right;
+            x2 = P.Left;
+            y1 = P.Top;
+            //y2 = P.Bottom;
+
+            x3 = G.Right;
+            x4 = G.Left;
+            y3 = G.Top;
+            //y4 = G.Bottom;
+
+            //w = P.Width;
+            h1 = P.Height;
+            h2 = G.Height;
+            if (x4 <= x1 && x4>=x2 && (y3 <= y1+h1 && y3+h2 >= y1))
+            {
+
+                lewo = false;
+                if(y3-h2<=y1)
+                {
+                    s = true;
+                    y3 = y1-h2;
+                }
+            }
+
+            if (x3 >= x2 && x3<=x1 && (y3 <= y1+h1 && y3+h2 >= y1))
+                prawo = false;
+
+            //while (x1 <= x3 + w / 2 && x1 >= x3 + w / 2)
+              //  lewo = false;
+
+          //  while (x1 <= x3 + w / 2 && x1 >= x3 + w / 2)
+            //    lewo = false;
+        }
+    
+        public Gra()
+        {
+            InitializeComponent();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            collision(Platform, Gracz, spadek);
+
+            if (skok  && spadanie )
+            {
+
+                
+                    Gracz.Top -= force;
+                
+                force +=1;
+ 
+                 
+                }
+            
+            if (Gracz.Top + Gracz.Height >= screen.Height&&!spadek)
+            {
+                Gracz.Top = screen.Height - Gracz.Height;
+                skok = false;
+                spadanie = false;
+
+            }
+            else if(!spadek)
+            {
+                Gracz.Top += mforce;
+                mforce = mforce+3;
+
+            }
+
+      
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            collision(Platform, Gracz, spadek);
+
+            if (prawo == true) { Gracz.Left += 1; }
+            if (lewo == true) { Gracz.Left -= 1; }
+
+            
+
+        }
+
+
+        private void Gra_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) {
+                prawo = true;
+                collision(Platform, Gracz, spadek);
+                 
+            }
+            if (e.KeyCode == Keys.Left) {
+                lewo = true;
+                collision(Platform, Gracz, spadek);
+                 
+            }
+            if (e.KeyCode == Keys.Escape) { this.Close(); }
+            if (!skok )
+            {
+
+                if ((e.KeyCode == Keys.Space && (!spadanie == true))) 
+                {
+                    //collision(Platform, Gracz);
+
+                    spadanie = true;
+                    skok = true;
+                    force = G;
+                      
+                    mforce = 1;
+
+                }
+            }
+        }
+
+        private void Gra_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) {
+                prawo = false;
+                collision(Platform, Gracz,spadek);
+                 
+            }
+            if (e.KeyCode == Keys.Left) {
+                lewo = false;
+                collision(Platform, Gracz, spadek);
+                
+            }
+            if (e.KeyCode == Keys.Space) {
+                skok = false; mforce = 1;
+                collision(Platform, Gracz,spadek);
+            }
+
+        }
+    }
+}
